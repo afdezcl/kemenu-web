5bdd72bfd84f32be2dedf276c19aae35242cfd73
afdezcl <adrifdez_@uma.es>
2021-05-29 14:34:25 +0200

    Add first ads banner in /show page

---
+++ kemenu-frontend/src/app/components/banner/banner.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from '../banner/banner.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [BannerComponent],
+  exports: [BannerComponent]
+})
+export class BannerModule { }
