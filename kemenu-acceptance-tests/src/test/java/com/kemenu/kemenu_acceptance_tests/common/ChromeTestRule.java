c5793064300f54deef2a93da1979ad902760b674
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-07-22 08:41:42 +0200

    Remove exoclick and increment wait time for acceptance tests

--- kemenu-acceptance-tests/src/test/java/com/kemenu/kemenu_acceptance_tests/common/ChromeTestRule.java
+++ kemenu-acceptance-tests/src/test/java/com/kemenu/kemenu_acceptance_tests/common/ChromeTestRule.java
@@ -18,7 +18,7 @@ public class ChromeTestRule extends ExternalResource {
     ChromeOptions chromeOptions = new ChromeOptions();
     chromeOptions.setHeadless(true);
     chrome = new ChromeDriver(chromeOptions);
-    chrome.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    chrome.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
   }
 
   @Override
