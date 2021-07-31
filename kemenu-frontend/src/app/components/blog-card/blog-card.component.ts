59443478a9104892e12a088b6e788990defc3f87
Adrián Fernández Claverías <adrian.claverias.sge@mango.com>
2021-07-31 16:53:32 +0200

    Link to the post and change css to scss blog card file

--- kemenu-frontend/src/app/components/blog-card/blog-card.component.ts
+++ kemenu-frontend/src/app/components/blog-card/blog-card.component.ts
@@ -5,7 +5,7 @@ import BlogPost from '@models/blog-post/blogPost';
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
-  styleUrls: ['./blog-card.component.css']
+  styleUrls: ['./blog-card.component.scss']
 })
 export class BlogCardComponent implements OnInit {
 
