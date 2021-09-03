ec4702aa238dc2edfe9a9c6309595a77762b21e6
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-09-03 09:50:53 +0200

    Return acceptance tests and fix kill node process

--- ci/kill_node_process.sh
+++ ci/kill_node_process.sh
@@ -1,2 +1,2 @@
-NODEKEMENUPID=$(ps aux | grep '\snode\s' | awk '{print $2}')
+NODEKEMENUPID=$(ps aux | grep '\s/root/node/bin/node\s' | awk '{print $2}')
 if [ -n "$NODEKEMENUPID" ]; then echo "Killing app with PID $NODEKEMENUPID" && kill -9 $NODEKEMENUPID; else echo 'App not running'; fi
