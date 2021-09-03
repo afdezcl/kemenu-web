ed6cf948ef8b258480968710e024502d1bc5cf96
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-09-03 12:03:17 +0200

    Removed unused controller

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/menu/MenuPublicController.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/menu/MenuPublicController.java
@@ -2,6 +2,7 @@ package com.kemenu.kemenu_backend.application.menu;
 
 import com.kemenu.kemenu_backend.application.customer.CustomerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Locale;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/public")
@@ -22,14 +24,17 @@ class MenuPublicController {
     @GetMapping("/short/{shortUrlId}")
     ResponseEntity<MenuResponse> readMenu(@PathVariable String shortUrlId, Locale locale) {
         return customerService.readMenus(shortUrlId, locale)
-                .map(menus -> ResponseEntity.ok(menus.get(0)))
-                .orElseGet(() -> ResponseEntity.notFound().build());
+            .map(menus -> ResponseEntity.ok(menus.get(0)))
+            .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/v1/short/{shortUrlId}")
     ResponseEntity<List<MenuResponse>> readMenus(@PathVariable String shortUrlId, Locale locale) {
         return customerService.readMenus(shortUrlId, locale)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+            .map(menus -> {
+                log.info("Showing {} shortUrl", shortUrlId);
+                return ResponseEntity.ok(menus);
+            })
+            .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
